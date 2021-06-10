3e7bdc81cb6c2a18d6f59bca5e9c6e1b34f45ff9
JessBonanno <jessbonanno@gmail.com>
2021-06-10 00:37:27 -0400

    added animated search

--- src/components/parts/search/index.tsx
+++ src/components/parts/search/index.tsx
@@ -1,10 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 import styles from './search.module.scss';
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <div className={styles.search}>
-      Search
+    <div className={styles.search} style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1618193319734-478296be37ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=377&q=80)' }}>
+      <h1>
+        <b>Explore</b>
+        {' '}
+        tons of shows, add favorites to track when to watch!
+      </h1>
+      <form action="">
+        <input type="search" name="searchTerm" value={searchTerm} placeholder="Search Shows" />
+        <BsSearch className={styles.icon} />
+      </form>
     </div>
   );
 };
