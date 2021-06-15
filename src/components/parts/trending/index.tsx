1e1c151bbb876e01724ec8a06b3efaae450cc6df
JessBonanno <jessbonanno@gmail.com>
2021-06-15 12:19:05 -0400

    playing w contaier heights

--- src/components/parts/trending/index.tsx
+++ src/components/parts/trending/index.tsx
@@ -39,7 +39,7 @@ export const Trending = () => {
       </div>
       <div>
         <InfiniteScroll
-          height="240px"
+          height="260px"
           className={styles.cardContainer}
           dataLength={(trending && trending.length) || []}
           next={getShows}
